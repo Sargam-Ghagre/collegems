c0fa7fce689cf8117861516d94cc25d335e12774
Mohammad-Hassan027 <shaikhmohammadhassan027@gmail.com>
2026-06-14 15:45:02 +0530

    Add logout route to authentication API

--- collegems-server/src/routes/auth.routes.js
+++ collegems-server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, refresh } from "../controllers/auth.controller.js";
+import { register, login, refresh, logout } from "../controllers/auth.controller.js";
 import { validateRegister } from "../middlewares/validation.middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", validateRegister, register);
 router.post("/login", login);
 router.post("/refresh", refresh);
+router.post("/logout", logout);
 
 export default router;
